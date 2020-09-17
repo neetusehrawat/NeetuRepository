e43c42efde21a7010fc5ca48f8fd972394a70dcb
neetu <neetu@192.168.0.22>
2020-09-16 20:31:20 -0500

    Updated Class name from validationCommand to ValidationCommandBing

--- NeetuTestProject/src/com/seleniumscripts/ValidationCommandsBing.java
+++ NeetuTestProject/src/com/seleniumscripts/ValidationCommandsBing.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class ValidationCommands {
+public class ValidationCommandsBing {
 	static boolean status;
 	static int i=0;
 	
